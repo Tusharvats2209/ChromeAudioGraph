b0b924c2677af1b3bc336592b2cb3d4e1870fcba
insani7yd <bkeshav2313@gmail.com>
2022-01-12 21:28:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ var audioCtx = new AudioContext(), source;
 document.getElementById('init').addEventListener("click", () => {audioCtx.resume();});   // this sucks
 
 var vid = document.querySelector('video');
-const oscillator = audioCtx.createOscillator();
-var sink = audioCtx.createMediaStreamDestination();
-oscillator.type = 'square';
-oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
-oscillator.connect(sink);
-oscillator.start();
+
+// connection won't persist without sending a dummy stream, need a better work around 
+const dummmyStream = audioCtx.createOscillator();
+var dummySink = audioCtx.createMediaStreamDestination();
+dummmyStream.type = 'square';
+dummmyStream.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+dummmyStream.connect(dummySink);
+dummmyStream.start();
 
 function hookNewNode(client) {
-  var call = peer.call(`${client.peerId}`,sink.stream);
+  var call = peer.call(`${client.peerId}`,dummySink.stream);
   call.on('stream', function(stream) {  
     vid.srcObject = stream;
     vid.onloadedmetadata = function(e) {
@@ -31,15 +33,3 @@ chrome.runtime.onMessage.addListener((message) => {
 
 // vid can dump previous srcObject to grab the new connection, the previous stream would still remain hooked to the web audio graph, cool little hack, huh!!
 // hope no vid elements are required in the future!
-
-// var call = peer.call('',sink.stream);
-// call.on('stream', function(stream) {  
-//   document.querySelector()
-//   vid.srcObject = stream;
-//   vid.onloadedmetadata = function(e) {
-//   vid.play();
-//     vid.muted = true;
-//   };
-//   source = audioCtx.createMediaStreamSource(stream);
-//   source.connect(audioCtx.destination);
-// });
